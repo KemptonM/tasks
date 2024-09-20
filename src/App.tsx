1dbe702dc6486360ca07da61d13688f877a4e81d
Kempton Maillett <kemptonmaillett@host-0800-849437de431a.pool.net.maine.edu>
2024-09-19 21:01:59 -0400

    make the list red

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,39 @@ function App(): React.JSX.Element {
             <h1>This is a header.</h1>
             <img src="../public/squirtle.jpg" alt="Squirtle with sunglasses" />
             <ol>
-                <li>Most important thing</li>
-                <li>Thing of average importance</li>
-                <li>An unimportant thing</li>
+                <li>
+                    Most important thing
+                    <div
+                        style={{
+                            width: "100px",
+                            height: "50px",
+                            backgroundColor: "red",
+                            marginTop: "10px",
+                        }}
+                    ></div>
+                </li>
+                <li>
+                    Thing of average importance
+                    <div
+                        style={{
+                            width: "100px",
+                            height: "50px",
+                            backgroundColor: "red",
+                            marginTop: "10px",
+                        }}
+                    ></div>
+                </li>
+                <li>
+                    An unimportant thing
+                    <div
+                        style={{
+                            width: "100px",
+                            height: "50px",
+                            backgroundColor: "red",
+                            marginTop: "10px",
+                        }}
+                    ></div>
+                </li>
             </ol>
             <p>Kempton was here! Hello World.</p>
             <Button
