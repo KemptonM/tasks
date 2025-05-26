fa1829ce4413d2afa4732c8c2fe74f1da94ef479
Kempton <kempton.maillett@maine.edu>
2025-05-25 23:29:26 -0400

    Update App component layout and add new features. Enhanced header with a welcome message, added placeholder image, list items, and a button to log a message. Integrated StartAttempt and CycleHoliday components for improved functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,38 @@ import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { TwoDice } from "./components/TwoDice";
 import { Counter } from "./components/Counter";
+import { StartAttempt } from "./components/StartAttempt";
+import { Button } from "react-bootstrap";
+import { CycleHoliday } from "./components/CycleHoliday";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+            <header
+                role="banner"
+                style={{ backgroundColor: "#e0e0e0", padding: "1rem" }}
+            >
+                <h1>Welcome to the Quiz App - COS420</h1>
             </header>
+            <div>Hello World</div>
+            <img src="https://via.placeholder.com/100" alt="Placeholder" />
+            <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <div
+                style={{ backgroundColor: "red", width: 50, height: 50 }}
+            ></div>
+            <hr />
+            <StartAttempt />
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -19,6 +44,8 @@ function App(): React.JSX.Element {
             <TwoDice></TwoDice>
             <hr />
             <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday />
         </div>
     );
 }
